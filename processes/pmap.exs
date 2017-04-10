87a7d9160f8be609f0766993a897507f01691706
Héctor Iván Patricio Moreno <hectorivanpatriciomoreno@gmail.com>
2017-04-09 23:59:31 -0500

    process

--- processes/pmap.exs
+++ processes/pmap.exs
@@ -1,23 +1,26 @@
 defmodule ParallelFake do
-  
-  def pmap(coleccion, funcion) do
-    mi_pid = self()
 
+  def pmap(coleccion, funcion) do
+    mi_pid = self() # obteniendo pid del proceso actual
     coleccion
       |> Enum.map(fn x ->
-        spawn(fn ->
-          # # :rand.seed(:os.timestamp)
-          # segs = :rand.uniform(2)
-          # :timer.sleep(segs * 1000)
-          # IO.puts (" #{x} : #{segs}")
-          send(mi_pid, {self, funcion.(x)}) 
+        spawn(fn ->  
+          random_wait()
+          send(mi_pid, {self(), funcion.(x)})
         end)
-      end)
-      |> Enum.map(fn pid -> 
+      end) # devuelve un mapa con la lista de PID's
+      |> Enum.map(fn pid ->
+        IO.puts "#{inspect(pid)}: Running"
         receive do
-          {^pid, result} -> result
+          {^pid, result} -> 
+            IO.puts "#{inspect(pid)}: Received"
+            result # ^ es llamado "pin operator", evita que una variable se reasigne en un pettern match
         end
-      end)
+      end) # devuelve la lista de valores calculados, en orden
+  end
 
+  def random_wait() do
+    segs = :rand.uniform(2)
+    :timer.sleep(segs * 1000)
   end
 end
